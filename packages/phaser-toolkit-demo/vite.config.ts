08a6e1370f52f2573a3746a2154d66ffa7f5e841
Renato Cassino <renato.santos@nsx.bet>
2025-08-16 15:19:57 -0300

    feat: add path aliasing for phaser-wind and font-awesome-for-phaser in Vite config

--- packages/phaser-toolkit-demo/vite.config.ts
+++ packages/phaser-toolkit-demo/vite.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   server: {
@@ -7,5 +12,14 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+  },
+  resolve: {
+    alias: {
+      'phaser-wind': resolve(__dirname, '../phaser-wind/src/index.ts'),
+      'font-awesome-for-phaser': resolve(
+        __dirname,
+        '../font-awesome-for-phaser/src/index.ts'
+      ),
+    },
   },
 });

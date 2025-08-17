5152d26aec42e42a3b01c9d069867d6a7d34992b
Cassino <renatocassino@gmail.com>
2025-08-17 16:11:09 -0300

    feat: uncomment exports in hudini plugin files and add button story example for PhaserWind

--- packages/hudini/src/plugin/plugin.ts
+++ packages/hudini/src/plugin/plugin.ts
@@ -1,103 +1,103 @@
-// import { Plugins } from 'phaser';
-// import {
-//   BaseThemeConfig,
-//   ThemeOverride
-// } from 'phaser-wind';
+import { Plugins } from 'phaser';
+import {
+    BaseThemeConfig,
+    ThemeOverride
+} from 'phaser-wind';
 
-// export const HUDINI_KEY: string = 'hudini';
+export const HUDINI_KEY: string = 'hudini';
 
-// /**
-//  * Plugin configuration data type.
-//  *
-//  * @typedef {Object} HudiniPluginData
-//  * @property {BaseThemeConfig} [theme] - Custom theme created using createTheme(). If not provided,
-//  *   the default light theme will be used
-//  * @property {boolean} [darkMode=false] - When true, uses the default dark theme. Only takes effect
-//  *   if no custom theme is provided
-//  */
-// export type HudiniPluginData<T = ThemeOverride> = {
-//   theme?: T;
-//   darkMode?: boolean;
-//   phaserWindMappingKey?: string; // default is 'pw'
-// };
+/**
+ * Plugin configuration data type.
+ *
+ * @typedef {Object} HudiniPluginData
+ * @property {BaseThemeConfig} [theme] - Custom theme created using createTheme(). If not provided,
+ *   the default light theme will be used
+ * @property {boolean} [darkMode=false] - When true, uses the default dark theme. Only takes effect
+ *   if no custom theme is provided
+ */
+export type HudiniPluginData<T = ThemeOverride> = {
+    theme?: T;
+    darkMode?: boolean;
+    phaserWindMappingKey?: string; // default is 'pw'
+};
 
-// /**
-//  * Phaser Wind Plugin class that manages theme configuration
-//  * @extends Plugins.BasePlugin
-//  */
-// export class HudiniPlugin<
-//   T extends BaseThemeConfig,
-// > extends Plugins.BasePlugin {
-//   // private themeInstance: T & BaseThemeConfig;
-//   // private phaserWindMappingKey: string;
+/**
+ * Phaser Wind Plugin class that manages theme configuration
+ * @extends Plugins.BasePlugin
+ */
+export class HudiniPlugin<
+    T extends BaseThemeConfig,
+> extends Plugins.BasePlugin {
+    // private themeInstance: T & BaseThemeConfig;
+    // private phaserWindMappingKey: string;
 
-//   /** Current theme configuration */
-//   // private theme: T & BaseThemeConfig;
+    /** Current theme configuration */
+    // private theme: T & BaseThemeConfig;
 
-//   /**
-//    * Creates an instance of HudiniPlugin
-//    * @param pluginManager - Phaser plugin manager instance
-//    */
-//   constructor(pluginManager: Plugins.PluginManager) {
-//     super(pluginManager);
+    /**
+     * Creates an instance of HudiniPlugin
+     * @param pluginManager - Phaser plugin manager instance
+     */
+    constructor(pluginManager: Plugins.PluginManager) {
+        super(pluginManager);
 
-//     // this.themeInstance = defaultLightTheme as T & BaseThemeConfig;
-//     // this.phaserWindMappingKey = 'pw';
-//   }
+        // this.themeInstance = defaultLightTheme as T & BaseThemeConfig;
+        // this.phaserWindMappingKey = 'pw';
+    }
 
-//   /**
-//    * Initializes the plugin with theme configuration
-//    * @param theme - Custom theme configuration
-//    * @param darkMode - Whether to use dark mode theme when no custom theme provided
-//    * @example
-//    * ```typescript
-//    * const game = new Phaser.Game({
-//    *   plugins: {
-//    *     global: [
-//    *       {
-//    *         key: HUDINI_KEY,
-//    *         plugin: HudiniPlugin,
-//    *         mapping: HUDINI_KEY,
-//    *         data: { theme: defaultLightTheme,  }
-//    *       },
-//    *     ],
-//    *   },
-//    * });
-//    * ```
-//    */
-//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-//   // override init({
-//   // theme,
-//   // darkMode = false,
-//   // phaserWindMappingKey = 'pw',
-//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-//   // }: HudiniPluginData<T>): void {
-//   // if (!theme) {
-//   //   this.theme = darkMode
-//   //     ? (defaultDarkTheme as T & BaseThemeConfig)
-//   //     : (defaultLightTheme as T & BaseThemeConfig);
-//   //   return;
-//   // } else {
-//   //   this.theme = theme as T & BaseThemeConfig;
-//   // }
+    /**
+     * Initializes the plugin with theme configuration
+     * @param theme - Custom theme configuration
+     * @param darkMode - Whether to use dark mode theme when no custom theme provided
+     * @example
+     * ```typescript
+     * const game = new Phaser.Game({
+     *   plugins: {
+     *     global: [
+     *       {
+     *         key: HUDINI_KEY,
+     *         plugin: HudiniPlugin,
+     *         mapping: HUDINI_KEY,
+     *         data: { theme: defaultLightTheme,  }
+     *       },
+     *     ],
+     *   },
+     * });
+     * ```
+     */
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // override init({
+    // theme,
+    // darkMode = false,
+    // phaserWindMappingKey = 'pw',
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // }: HudiniPluginData<T>): void {
+    // if (!theme) {
+    //   this.theme = darkMode
+    //     ? (defaultDarkTheme as T & BaseThemeConfig)
+    //     : (defaultLightTheme as T & BaseThemeConfig);
+    //   return;
+    // } else {
+    //   this.theme = theme as T & BaseThemeConfig;
+    // }
 
-//   // this.phaserWindMappingKey = phaserWindMappingKey;
+    // this.phaserWindMappingKey = phaserWindMappingKey;
 
-//   // this.themeInstance = {
-//   //   ...this.themeInstance,
-//   //   ...this.theme,
-//   // } as T & BaseThemeConfig;
+    // this.themeInstance = {
+    //   ...this.themeInstance,
+    //   ...this.theme,
+    // } as T & BaseThemeConfig;
+}
+
+/**
+ * Returns the current theme configuration
+ * @returns Current BaseThemeConfig
+ */
+// publsic getTheme(): T & BaseThemeConfig {
+// return this.theme;
 // }
 
-// /**
-//  * Returns the current theme configuration
-//  * @returns Current BaseThemeConfig
-//  */
-// // publsic getTheme(): T & BaseThemeConfig {
-// // return this.theme;
-// // }
-
-// // public get pw(): T & BaseThemeConfig {
-// //   return this.themeInstance;
-// // }
-// // }
+// public get pw(): T & BaseThemeConfig {
+//   return this.themeInstance;
+// }
+// }
--- packages/phaser-wind/src/components/column.ts
+++ packages/phaser-wind/src/components/column.ts
@@ -1,5 +1,1 @@
-// export class Column extends Phaser.GameObjects.Container {
-//   constructor(scene: Scene, x: number, y: number, width: number, height: number, children: GameObjects.Container[]) {
-//     super(scene, x, y, width, height);
-//   }
-// }
+export {}

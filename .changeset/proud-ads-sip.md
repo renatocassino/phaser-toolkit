83965bf9167b2d1f95fd00c25b570a726742af30
Renato Cassino <renatocassino@gmail.com>
2025-08-28 23:17:57 -0300

    Feature/get hudini function (#112)
    * feat: add phaser-sound-studio package with ESLint configuration, README, and initial setup
    
    * feat: add phaser-sound-studio package with ESLint configuration, README, and initial setup
    
    * feat: add start plugin
    
    * feat: wip - add sounds in storybook
    
    * feat: first example
    
    * feat: add start plugin
    
    * fix: global state in phaser-hooks
    
    * feat: add example to phaser-hooks
    
    * feat: add examples to phaser-hooks with persistent stories
    
    * feat: change examples in phaser-hooks
    
    * feat: add lazy load and typesafe to play
    
    * feat: enhance Phaser Sound Studio with type-safe configurations and channel management
    
    * feat: add playOnce method to PhaserSoundStudioPlugin for conditional sound playback
    
    * feat: add changeset
    
    * feat: add bug fix safe
    
    * feat: update README
    
    * fix: build
    
    * feat: add another step to check storybook
    
    * fix: build storybook in node 18
    
    * feat: get hudini function
    
    * feat: update changeset

---
+++ .changeset/proud-ads-sip.md
@@ -0,0 +1,7 @@
+---
+'phaser-sound-studio': minor
+'phaser-hooks': patch
+---
+
+add channels in plugin
+fix global hooks
--- packages/hudini/src/utils/get-pw-from-scene.ts
+++ packages/hudini/src/utils/get-pw-from-scene.ts
@@ -1,6 +1,19 @@
 import { Scene } from 'phaser';
-import { PhaserWindPlugin, SceneWithPhaserWind } from 'phaser-wind';
+import {
+  BaseThemeConfig,
+  PhaserWindPlugin,
+  SceneWithPhaserWind,
+} from 'phaser-wind';
+
+import { HudiniPlugin } from '../plugin/plugin';
+import { SceneWithHudini } from '../scene/scene-with-hudini';
 
 export const getPWFromScene = (scene: Scene): PhaserWindPlugin<{}> => {
   return (scene as unknown as SceneWithPhaserWind<{}>).pw;
+};
+
+export const getHudini = <T extends BaseThemeConfig = BaseThemeConfig>(
+  scene: Scene
+): HudiniPlugin<T> => {
+  return (scene as unknown as SceneWithHudini<T>).hudini;
 };

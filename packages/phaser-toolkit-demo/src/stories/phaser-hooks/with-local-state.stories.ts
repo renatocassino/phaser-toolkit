a11ea0adc48f21bc91771f58a00676e5094b8420
Cassino.dev <renatocassino@gmail.com>
2025-09-19 11:55:58 -0300

    feat: add on-change-example

--- packages/phaser-toolkit-demo/src/stories/phaser-hooks/with-local-state.stories.ts
+++ packages/phaser-toolkit-demo/src/stories/phaser-hooks/with-local-state.stories.ts
@@ -8,6 +8,7 @@ import { cleanGames, createGame } from '../helpers/create-game';
 import { nextFrames } from '../helpers/next-tick';
 import { PreviewScene, PreviewScene2 } from './with-local-state/simple-example';
 import { defaultLightTheme, HUDINI_KEY, HudiniPlugin, HudiniPluginData } from 'hudini';
+import { PreviewScene2OnChange, PreviewSceneOnChange } from './with-local-state/on-change-example';
 
 const ID = 'phaser-hooks-game-with-local-state';
 
@@ -39,6 +40,40 @@ export const BasicExample: StoryObj = {
             parent: ID,
             backgroundColor: '#2c3e50',
             scene: [PreviewScene, PreviewScene2],
+            plugins: {
+                global: [
+                    {
+                        key: HUDINI_KEY,
+                        plugin: HudiniPlugin,
+                        mapping: HUDINI_KEY,
+                        start: true,
+                        data: {
+                            theme: defaultLightTheme,
+                        } as HudiniPluginData,
+                    }
+                ]
+            },
+        });
+        await nextFrames(1);
+    },
+};
+
+export const ListenerOnChange: StoryObj = {
+    render: (): HTMLElement => {
+        const root = document.createElement('div');
+        root.id = ID;
+        return root;
+    },
+    play: async (): Promise<void> => {
+        await cleanGames();
+        await nextFrames(1);
+        createGame(ID, {
+            type: Phaser.AUTO,
+            width: 800,
+            height: 600,
+            parent: ID,
+            backgroundColor: '#2c3e50',
+            scene: [PreviewSceneOnChange, PreviewScene2OnChange],
             plugins: {
                 global: [
                     {

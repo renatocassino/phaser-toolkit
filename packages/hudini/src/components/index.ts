ef9483c758f028ae8c98a61fe3ae1a53e7960c75
Renato Cassino <renatocassino@gmail.com>
2025-11-10 21:25:37 -0300

    Feature/hudini features (#220)
    * feat: implement Column and Row layout components for Phaser
    
    * fix: add column and row to components
    
    * feat: add circular progress page
    
    * fix: card size
    
    * feat: add pages to progress
    
    * feat: add section header component
    
    * feat: add icons in icon-button from hudini
    
    * feat: add page flat-icon-button
    
    * feat: add page sized-box
    
    * feat: add panel in export
    
    * feat: add method to update size of card
    
    * feat: add changeset

--- packages/hudini/src/components/index.ts
+++ packages/hudini/src/components/index.ts
@@ -1,11 +1,14 @@
 export * from './card';
 export * from './circular-progress';
-export * from './column';
 export * from './flat-icon-button';
 export * from './icon-button';
 export * from './linear-progress';
+export * from './panel';
 export * from './radial-progress';
-export * from './row';
 export * from './section-header';
 export * from './sized-box';
 export * from './text-button';
+
+// Re-export Row and Column from phaser-wind
+export { Column, Row } from 'phaser-wind';
+export type { ColumnParams, HorizontalAlign, RowParams, VerticalAlign } from 'phaser-wind';
